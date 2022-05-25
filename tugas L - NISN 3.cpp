7869eedec4b52b98b7218ee9c03b07c849751a4d
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-05-25 11:16:44 +0800

    Create tugas L - NISN 3.cpp

---
+++ tugas L - NISN 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    int a,b,x,y;
+    
+    cout << "masukan nilai x: ";
+    cin >> a;
+    
+    cout << "masukan nilai y: ";
+    cin >> b;
+    
+    x = b;
+    y = a;
+    
+    cout << "nilai x : " << x << endl;
+    cout << "nilai y : " << y << endl;
+    
+    return 0;
+}
